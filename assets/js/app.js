1a356ca5c47ceeee15f7df89c3ba74eb5c9490b3
Michael Salvati <print@3daddict.com>
2018-11-14 13:41:12 -0800

    glass effect integrated

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+$(document).ready(InitApp);
+
+let crackSize = 60;
+let crackRadius = 150;
+
+function InitApp() {
+  glassCrack();
+  if ( $(window).width() > 768) {      
+    document.getElementById("target-img").src="./assets/img/glassTexture.png";
+    crackSize = 60;
+    crackRadius = 150;
+  } 
+  else {
+    document.getElementById("target-img").src="./assets/img/glassTextureMd.png";
+    crackSize = 40;
+    crackRadius = 100;
+  }
+}
+
+function glassCrack() {
+  let crackCount = 0;
+  let preLoad = document.getElementsByClassName("preload")[0];
+  preLoad.style.display = "none";
+
+  function createCrack(top, left) {
+    let className = "crack" + crackCount;
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", className + " crack-container");
+    crackCount++;
+
+    let innerCrack = document.createElement("div");
+    if (crackCount === 1) {
+      innerCrack.setAttribute("class", "crack");
+    }
+    if (crackCount === 2) {
+      innerCrack.setAttribute("class", "crack-2");
+    }
+    if (crackCount === 3) {
+      $(".crack-container").remove();
+      explode();
+      $(".explode-wrapper").fadeOut(2000);
+	}
+	if (crackCount === 4) {
+		return;
+	}
+    newDiv.appendChild(innerCrack);
+    document.getElementById("target").appendChild(newDiv);
+    newDiv.style.top = top + "px";
+    newDiv.style.left = left + "px";
+  }
+
+  window.addEventListener("click", function(e) {
+    createCrack(e.pageY, e.pageX);
+  });
+
+}
+
+/* Function to explode the glassTexture.png */
+function explode() {
+  $("#target-img").explodeRestore();
+  $("#target-img").explode({
+    maxWidth: 12,
+    minWidth: crackSize,
+    radius: crackRadius,
+    release: false,
+    recycle: false,
+    explodeTime: 30,
+    canvas: true,
+    maxAngle: 360,
+    gravity: true
+  });
+}
